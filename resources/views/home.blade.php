380b10dc09d2fc3b07ceb3605186ce9027ad5732
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-10 14:55:56 +0200

    style: add columns

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,20 @@
 
 @section('content')
   <div class="container">
-    @foreach($movies as $singleMovie)
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">{{$singleMovie->title}}</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">{{$singleMovie->original_title}}</h6>
-          <em class="card-text">{{$singleMovie->nationality}}</em><br>
-          <span class="card-text">{{$singleMovie->date}}</span><br>
-          <b class="card-text">{{$singleMovie->vote}}</b>
+    <div class="row">
+      @foreach($movies as $singleMovie)
+        <div class="col-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{{$singleMovie->title}}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">{{$singleMovie->original_title}}</h6>
+              <em class="card-text">{{$singleMovie->nationality}}</em><br>
+              <span class="card-text">{{$singleMovie->date}}</span><br>
+              <b class="card-text">{{$singleMovie->vote}}</b>
+            </div>
+          </div>
         </div>
-      </div>
-    @endforeach
+      @endforeach
+    </div>
   </div>
 @endsection
