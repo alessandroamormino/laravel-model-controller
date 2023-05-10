d26bd170cf951e57111355a00f5e67b141676f99
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-10 14:54:04 +0200

    feat: print in page db table movies records

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-      return view('home');
+      // mi memorizzo tutti dati dei movies in un oggetto che prendo dal db
+      $movies = Movie::all();
+
+      return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layout/main')
 
 @section('content')
-  Contenuto
+  <div class="container">
+    @foreach($movies as $singleMovie)
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{{$singleMovie->title}}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">{{$singleMovie->original_title}}</h6>
+          <em class="card-text">{{$singleMovie->nationality}}</em><br>
+          <span class="card-text">{{$singleMovie->date}}</span><br>
+          <b class="card-text">{{$singleMovie->vote}}</b>
+        </div>
+      </div>
+    @endforeach
+  </div>
 @endsection
